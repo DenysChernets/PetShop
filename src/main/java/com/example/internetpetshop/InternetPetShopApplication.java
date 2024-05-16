da51a80bbf0990dc3b9afccf91af94517df53200
IshchenkoDan <ishchenko44@gmail.com>
2024-05-16 19:18:40 +0300

    Project commit

---
+++ src/main/java/com/example/internetpetshop/InternetPetShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.internetpetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetPetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetPetShopApplication.class, args);
+	}
+
+}
