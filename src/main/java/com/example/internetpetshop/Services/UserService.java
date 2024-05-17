c3b77aeda2bc32e44796b9386458ce30e23f963b
IshchenkoDan <ishchenko44@gmail.com>
2024-05-17 14:32:28 +0300

    PS-16 add Login controller and add login methods in service

--- src/main/java/com/example/internetpetshop/Services/UserService.java
+++ src/main/java/com/example/internetpetshop/Services/UserService.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public interface UserService {
     void save(User user);
 
-   // void login(String username, String password);
+    void login(String username, String password);
 
     User findByUserName(String username);
 
